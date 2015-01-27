e4ac2db4e4f178c026fdbedc9f9963ee2d10d731
LoiWu <superloiwu@gmail.com>
2015-01-27 17:09:20 +0800

    Task E: A Smarter Cart
    Iteration E1: Creating a Smarter Cart
    
    enables you to express this algorithm. With this code in place, we
    apply this migration just like any other migration.
    Mall> rake db:migrate
    ==  CombineItemsInCart: migrating
    =============================================
    ==  CombineItemsInCart: migrated (0.0448s)
    ====================================

--- Mall/db/schema.rb
+++ Mall/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127083429) do
+ActiveRecord::Schema.define(version: 20150127085817) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
