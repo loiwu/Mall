1b823193e912d1adb4a47f33a29ad91d4e40da6a
LoiWu <superloiwu@gmail.com>
2015-01-27 16:57:03 +0800

    Task E: A Smarter Cart
    Iteration E1: Creating a Smarter Cart
    
    /app/controllers/line_items_controller.rb
    /app/views/carts/show.html.erb

--- Mall/app/models/cart.rb
+++ Mall/app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
-  def
-    current_item = line_itmes.find_by(product_id: product_id)
+  def add_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
     if current_item
       current_item.quantity += 1
     else
